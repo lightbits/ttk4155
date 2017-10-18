57d7ef7aa7d1a801be80ea44de2d37b43aaff971
lightbits <simenh.jobb@gmail.com>
2017-10-18 07:47:05 +0200

    added write_many and read_many to header file

--- code/src/mcp2515.h
+++ code/src/mcp2515.h
@@ -6,6 +6,8 @@
 int mcp_init(void);
 uint8_t mcp_read(uint8_t address);
 int mcp_write(uint8_t address, uint8_t data);
+int mcp_write_many(uint8_t address, uint8_t *data, uint8_t count);
+void mcp_read_many(uint8_t address, uint8_t *data, uint8_t count);
 int mcp_bit_modify(uint8_t address, uint8_t mask, uint8_t data);
 int mcp_reset(void);
 uint8_t mcp_read_status(void);
