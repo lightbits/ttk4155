1bab6b7902f9a7b0bf6f405fae15fc1a3fc0ba20
Shared Computer <simeh@itk-d671.win.ntnu.no>
2017-10-25 16:04:40 +0200

    IR and ADC works

--- code_node2/code_node2/main.c
+++ code_node2/code_node2/main.c
@@ -131,6 +131,94 @@ void test_pwm()
 	}
 }
 
+// max effect = 150 mW
+// max voltage = 0.42 V (no light)
+// -> max current = I = P/U = 150 mW / 0.42V = 0.357 A
+
+// the resistor needs to make the current in the circuit equal to this
+// voltage over resistor is 5V - 0.42V = 4.58V
+// U = RI -> R = U/I = 4.58 / 0.357 = 13 ohm
+
+// this will read the voltage connected to ADC channel on pin 
+void test_ir_adc()
+{
+	uart_init(9600);
+	printf("testing IR and adc\n");
+
+	ADCSRA = (1 << ADEN);
+
+	// Use AVCC as voltage source
+	ADMUX = (1 << REFS0);
+
+	// ADC0 is source channel by default
+
+	const int num_past_values = 20;
+	uint16_t past_values[num_past_values];
+
+	int score = 0;
+	int was_under_threshold = 0;
+
+	while (1)
+	{
+		set_bit(ADCSRA, ADSC); // Start conversion
+		while (test_bit(ADCSRA, ADSC)) ; // wait until done
+
+		uint16_t x = ADC;
+
+		for (int i = 0; i < num_past_values - 1; i++)
+			past_values[i] = past_values[i+1];
+		past_values[num_past_values-1] = x;
+
+		uint16_t sum = 0;
+		for (int i = 0; i < num_past_values; i++)
+			sum += past_values[i];
+		uint16_t avg = sum / num_past_values;
+
+		if (avg < 10)
+		{
+			if (was_under_threshold) { }
+			else
+			{
+				score += 1;
+				was_under_threshold = 1;
+			}
+		}
+		else
+		{
+			was_under_threshold = 0;
+		}
+			
+
+		printf("%d %d\n", avg, score);
+
+		_delay_ms(100);
+	}
+}
+
+void test_can_joystick_servo()
+{
+	uart_init(9600);
+	mcp_init();
+	mcp_mode_normal();
+	pwm_init(50);
+	printf("(node 2) Testing servo control with joystick via CAN...\n");
+
+	while (1)
+	{
+		uint16_t id;
+		uint8_t data[8];
+		uint8_t length;
+		while (!mcp_read_message(&id, data, &length)) { }
+		uint8_t joy_x = data[0];
+		uint8_t joy_y = data[1];
+
+		float position = (float)(joy_x-28)/255;
+		servo_position(position);
+
+		printf("(node 2) %d %d\n", joy_x, joy_y);
+	}
+}
+
 int main(void)
 {
 	// uart_test();
@@ -138,5 +226,7 @@ int main(void)
 	// test_can_loopback();
 	// test_can_between_nodes();
 	// test_can_and_joystick();
-	test_pwm();
+	// test_pwm();
+	// test_can_joystick_servo();
+	test_ir_adc();
 }
