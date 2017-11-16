ec5a29d72dc286d161fa74f98e27eb44c5c79928
SharedComputer <simeh@ITK-D651.win.ntnu.no>
2017-11-16 11:29:43 +0100

    spi driver comments

--- code/src/spi.c
+++ code/src/spi.c
@@ -6,39 +6,24 @@
 #define SPI_MISO PB6
 #define SPI_SCK PB7
 
-
-int spi_init(void) {
+void spi_init(void) {
 	// Set MOSI, SCK and SS output, all others input
 	DDRB = (1<<SPI_MOSI) | (1<<SPI_SCK) | (1<<SPI_SS);
 	
 	// Enable SPI, Master, set clock rate fck/16
 	SPCR = (1<<MSTR) | (1<<SPR0);
 	SPCR |= (1<<SPE);
-	
-	return 0;
 }
-
 
 uint8_t spi_write(char data) {
- 	// Start transmission
- 	SPDR = data;
-
- 	// Wait for transmission complete
- 	while(!(SPSR & (1<<SPIF)));
-
- 	return SPDR;
+ 	SPDR = data; // write byte and start transmission
+ 	while(!(SPSR & (1<<SPIF))); // wait until transmission is done
+ 	return SPDR; // return data that was shifted in simultaneously from slave
 }
-
 
 uint8_t spi_read(void) {
- 	// Wait reception complete
-	 SPDR = 0xDC;
- 	while(!(SPSR & (1<<SPIF)));
-
-	// Return data register
- 	return SPDR;
+	return spi_write(0xDC); // write a dummy byte and return the slave data
 }
-
 
 void spi_slave_select(void) {
  	// Set !SS to 0 for activation of slave selection
--- code/src/spi.h
+++ code/src/spi.h
@@ -2,7 +2,7 @@
 #define SPI_H_
 #include "common.h"
 
-int spi_init(void);
+void spi_init(void);
 uint8_t spi_write(char data);
 uint8_t spi_read(void);
 void spi_slave_select(void);
