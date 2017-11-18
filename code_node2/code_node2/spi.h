cd13d68d5dbf768d7431f84e256bb07ec7f712fe
lightbits <simenh.jobb@gmail.com>
2017-11-18 12:51:39 +0100

    spi header and c file

--- code_node2/code_node2/spi.h
+++ code_node2/code_node2/spi.h
@@ -1,27 +1,7 @@
-// This enables SPI on the ATmega2560 to communicate with the MCP2510
-// on the I/O board. Polarity and Phase are 0,0. Rate is F_CPU/16.
-// See page 193 for example code which the below is based on.
 #ifndef SPI_H
 #define SPI_H
-#define SPI_MISO   PB3
-#define SPI_MOSI   PB2
-#define SPI_SCK    PB1
-#define SPI_SS     PB0 // Page 195: This must be set as output for SPI to work.
-#define MCP2510_SS PB7
-void spi_init()
-{
-    DDRB = (1<<SPI_MOSI) | (1<<SPI_SCK) | (1<<SPI_SS) | (1<<MCP2510_SS);
-    SPCR = (1<<MSTR) | (1<<SPR0);
-    SPCR |= (1<<SPE); // For some reason we need to |= this in after the above.
-}
-uint8_t spi_write(uint8_t data)
-{
-    SPDR = data;
-    while(!(SPSR & (1<<SPIF)));
-    return SPDR;
-}
-uint8_t spi_read()
-{
-    return spi_write(0xDC);
-}
+#include "common.h"
+void spi_init();
+uint8_t spi_write(uint8_t data);
+uint8_t spi_read();
 #endif
