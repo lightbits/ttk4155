6695b942da6d03f2a59751a1c5e3a01b5fb5b352
SharedComputer <simeh@itk-d673.win.ntnu.no>
2017-11-18 16:11:00 +0100

    message comments

--- shared.h
+++ shared.h
@@ -7,13 +7,13 @@ const uint8_t MODE_MENU = 3;
 const uint8_t MODE_LOST = 4;
 typedef struct 
 {
-	uint8_t angle;
-	uint8_t position;
-	uint8_t shoot;
-	uint8_t mode;
+	uint8_t angle; // joystick or remote controller angle
+	uint8_t position; // position of slider or (nothing at all!)
+	uint8_t shoot; // 1 (active) or 0 (inactive) joystick button or remote button
+	uint8_t mode; // game mode
 } can_msg_node1_to_node2;
 typedef struct 
 {
-	uint8_t light_blocked;
+	uint8_t light_blocked; // 1 (blocked) or 0 (not blocked)
 } can_msg_node2_to_node1;
 #endif
