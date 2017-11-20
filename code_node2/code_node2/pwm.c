0d35e39192e7e1c03de64a218287eafbb0d9290c
lightbits <simenh.jobb@gmail.com>
2017-11-20 10:58:12 +0100

    pwm documentation

--- code_node2/code_node2/pwm.c
+++ code_node2/code_node2/pwm.c
@@ -13,16 +13,27 @@ void pwm_init(uint32_t frequency)
 	set_bit(TCCR1A, COM1B1);
 	set_bit(TCCR1A, COM1B0);
 
-	// Set pin as output (see pinout overview)
-	set_bit(DDRB, DDD6);
+	// Set pin as output
+	set_bit(DDRB, 6);
 
-	// Hardcode N value to be 8 (this divides clock frequency by 8)
-	clear_bit(TCCR1B, CS12);
-	set_bit(TCCR1B, CS11);
-	clear_bit(TCCR1B, CS10);
+	// Set the clock divisor and start clock
+	#define N PWM_CLOCK_DIVISOR
+	#if N==1
+	TCCR1B = (TCCR1B & 0b11111000) | 0b001; // F_CPU/1
+	#elif N==8
+	TCCR1B = (TCCR1B & 0b11111000) | 0b010; // F_CPU/8
+	#elif N==64
+	TCCR1B = (TCCR1B & 0b11111000) | 0b011; // F_CPU/64
+	#elif N==256
+	TCCR1B = (TCCR1B & 0b11111000) | 0b100; // F_CPU/256
+	#elif N==1024
+	TCCR1B = (TCCR1B & 0b11111000) | 0b101; // F_CPU/1024
+	#else
+	#error "PWM_CLOCK_DIVISOR must be one of 1,8,64,256 or 1024"
+	#endif
 
 	// Set TOP value
-	uint16_t TOP = (uint16_t)( (F_CPU/8)/frequency - 1);
+	uint16_t TOP = (uint16_t)( (F_CPU/PWM_CLOCK_DIVISOR)/frequency - 1);
 	OCR1A = TOP;
 }
 
@@ -34,7 +45,7 @@ void pwm_duty(float duty_cycle)
 void pwm_pulse_duration(float seconds)
 {
 	uint32_t TOP = (uint32_t)OCR1A;
-	uint32_t frequency = F_CPU/(8*(1+TOP));
+	uint32_t frequency = F_CPU/(PWM_CLOCK_DIVISOR*(1+TOP));
 	pwm_duty(seconds*frequency);
 }
 
--- code_node2/code_node2/pwm.h
+++ code_node2/code_node2/pwm.h
@@ -1,12 +1,41 @@
-// This enables PWM signal generation on PB6/OC1B on the arduino.
-// The prescaler used is hardcoded to 8, meaning that the maximum
-// freqency is 2MHz (F_CPU/8) and the minimum frequency is 30 Hz
-// (TOP = 65535).
+// This enables PWM output on PB6/OC1B on the ATmega2560.
+// The range of acceptable frequencies depends on PWM_CLOCK_DIVISOR
+// (one of 1, 8, 256 or 1024). See note below.
 #ifndef PWM_H
 #define PWM_H
+#ifndef PWM_CLOCK_DIVISOR
+#define PWM_CLOCK_DIVISOR 8
+#endif
 #include "common.h"
 void pwm_init(uint32_t frequency); // between 30 and 2 000 000
 void pwm_duty(float duty_cycle); // between 0.0f and 1.0f
 void pwm_pulse_duration(float seconds);
 void pwm_pulse_duration_ms(float milliseconds);
 #endif
+
+//
+//                    F_CPU = 16 MHz
+//    N  | Min frequency | Max frequency |  Max period |
+// ------+---------------+---------------+-------------+
+//    1  |     244 Hz    |       16 MHz  |    4.096 ms |
+//    8  |    30.5 Hz    |        2 MHz  |   32.768 ms |
+//   64  |    3.81 Hz    |     0.25 MHz  |  262.144 ms |
+//  256  |    0.95 Hz    |     62.5 kHz  | 1048.576 ms |
+// 1024  |    0.24 Hz    |     15.6 kHz  | 4194.304 ms |
+//
+// To generate a PWM signal the MCU has a counter that goes from 0 to TOP.
+// When the counter is above a set threshold, the output is 1, and otherwise
+// 0. The duty cycle (0-100%) can then be adjusted by varying this threshold
+// between 0 and TOP.
+//
+// The period of the signal (or its frequency) is defined by the clock
+// (how fast to count) and TOP (how long to count). The clock source can
+// be divided by N=1, 8, 64, 256 or 1024. In other words:
+//
+//   frequency = (F_CPU/N)/TOP
+//
+// You choose N such that the TOP is between 0 and 65535 for frequencies in
+// the range you are interested in. For a given N, the frequency range is
+//   min = (F_CPU/N)/65535
+//   max = F_CPU/N
+//
