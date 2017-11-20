77af94de48de60fec210f2746647b5eae1fba249
lightbits <simenh.jobb@gmail.com>
2017-11-20 10:54:09 +0100

    set wrong register in music.c

--- code_node2/code_node2/music.c
+++ code_node2/code_node2/music.c
@@ -26,15 +26,15 @@ void wave_frequency(uint32_t frequency)
 		#define N MUSIC_CLOCK_DIVISOR
 		#define N 8
 		#if N==1
-		TCCR1B = (TCCR1B & 0b11111000) | 0b001; // F_CPU/1
+		TCCR3B = (TCCR3B & 0b11111000) | 0b001; // F_CPU/1
 		#elif N==8
-		TCCR1B = (TCCR1B & 0b11111000) | 0b010; // F_CPU/8
+		TCCR3B = (TCCR3B & 0b11111000) | 0b010; // F_CPU/8
 		#elif N==64
-		TCCR1B = (TCCR1B & 0b11111000) | 0b011; // F_CPU/64
+		TCCR3B = (TCCR3B & 0b11111000) | 0b011; // F_CPU/64
 		#elif N==256
-		TCCR1B = (TCCR1B & 0b11111000) | 0b100; // F_CPU/256
+		TCCR3B = (TCCR3B & 0b11111000) | 0b100; // F_CPU/256
 		#elif N==1024
-		TCCR1B = (TCCR1B & 0b11111000) | 0b101; // F_CPU/1024
+		TCCR3B = (TCCR3B & 0b11111000) | 0b101; // F_CPU/1024
 		#else
 		#error "MUSIC_CLOCK_DIVISOR must be one of 1,8,64,256 or 1024"
 		#endif
