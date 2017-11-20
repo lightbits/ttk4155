33dab34b60f1a7dd3b819a3e1a4783e0c8a92cba
lightbits <simenh.jobb@gmail.com>
2017-11-20 11:06:56 +0100

    ADC_MEMORY_START instead of 0x1400 hardcoded

--- code/src/adc.c
+++ code/src/adc.c
@@ -3,7 +3,7 @@
 // channel is one of 0 (ch1), 1 (ch2), 2 (ch3), 3 (ch4)
 uint8_t adc_read(int channel)
 {
-    volatile uint8_t *ADC = (uint8_t*)0x1400; // should maybe be global const
+    volatile uint8_t *ADC = (uint8_t*)ADC_MEMORY_START;
     uint8_t mux_address = 4+channel;
     *ADC = mux_address;
     _delay_us(50); // Maximum conversion time is 40 microseconds (t_c)
